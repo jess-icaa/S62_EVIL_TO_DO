e09ae93f583a92ecf45c266f1fffb1376c76b65b
jess-icaa <jessicashalominselvan@JESSICAs-Laptop.local>
2025-01-05 22:00:15 +0530

    renderr

--- server.js
+++ server.js
@@ -1,28 +1,24 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; 
 
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the EVIL TO-DO LIST!');
+});
 
 app.get('/ping', (req, res) => {
     res.send('pong');
 });
 
-app.get('/tasks', (req, res) => {
-    res.send('Welcome to the EVIL TO-DO LIST!');
-});
-
-app.post('/generate-task', (req,res) => {
-    res.send({ task: 'Rearrange your books by size. '});
-});
-
-app.get('/sarcastic-quote', (req, res) => {
-    res.send({ status: 'Sabotage Mode Activated. Good luck finding your tasks!' });
+app.post('/generate-task', (req, res) => {
+    res.send({ task: 'Rearrange your books by size.' });
 });
 
 app.get('/sarcastic-quote', (req, res) => {
     const quotes = [
-        "Congrats! You did the bare minimum",
+        "Congrats! You did the bare minimum.",
         "You're one step closer to being mediocre.",
         "Great! Another task done... for now.",
     ];
@@ -30,6 +26,12 @@ app.get('/sarcastic-quote', (req, res) => {
     res.send({ quote: randomQuote });
 });
 
+
+app.get('/sabotage-mode', (req, res) => {
+    res.send({ status: 'Sabotage Mode Activated. Good luck finding your tasks!' });
+});
+
 app.listen(port, () => {
     console.log(`EVIL TO-DO LIST is running on http://localhost:${port}`);
 });
+
